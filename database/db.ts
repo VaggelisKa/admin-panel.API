f9b7f2e9e8ef67c2fa450f7b45d737ac365180dc
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2020-12-26 19:03:15 +0200

    Connecting to postgres through docker image

--- database/db.ts
+++ database/db.ts
@@ -1,11 +1,15 @@
 import { Client } from "https://deno.land/x/postgres@v0.4.6/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = config();
 
 export const client = new Client({
-    database: "simple_panel",
-    user: "simple_admin",
-    password: "4567891452",
-    hostname: "localhost",
-    port: 5432
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    hostname: DB_HOST,
+    port: +DB_PORT
 });
+
+
 
--- index.ts
+++ index.ts
@@ -2,10 +2,14 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import { GraphQLService } from './server.ts';
 
+import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+
+const { PORT } = config();
+
 const app = new Application();
 
 app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
 
 console.log("deno server running!!");
 
-await app.listen({ port: 8000 });
+await app.listen({ port: +PORT });
